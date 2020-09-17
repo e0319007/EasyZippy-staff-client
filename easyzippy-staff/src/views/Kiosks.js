fefe3f87781b69958bac2360458cc6a43aad1326
eileen <e0319007@u.nus.edu>
2020-09-17 10:13:29 +0800

    [eileen_leong/ch92/staff-category-crud] category crud

--- easyzippy-staff/src/views/Kiosks.js
+++ easyzippy-staff/src/views/Kiosks.js
@@ -1,5 +1,4 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 import {
     Card,
@@ -7,17 +6,18 @@ import {
     CardTitle,
     Row,
     Col,
-    Form,
-    FormGroup,
     Input,
     Button,
-    Table,
     CardHeader, InputGroup, InputGroupText, InputGroupAddon
 } from "reactstrap";
 import CreateKioskForm from "./CreateKioskForm";
 import UseTable from "../components/UseTable";
 import * as KioskService from "../services/kioskService";
 import Popup from "../components/Popup"
+import { AiOutlineEdit } from 'react-icons/ai';
+import { AiOutlineClose } from 'react-icons/ai';
+import Noti from "../components/Noti";
+import ConfirmModal from "../components/ConfirmModal";
 
 const headCells = [
     {id:'id', label:'ID'},
@@ -27,13 +27,17 @@ const headCells = [
     {id:'enabled', label:'Enabled'},
     //if want to disable sorting for a particular column 
     //{id:'enabled', label:'Enabled', disableSorting:true},
+    {id:'actions', label:'Actions', disableSorting:true}
 ]
 
 function Kiosks() {
 
+    const [recordForEdit, setRecordForEdit] = useState(null)
     const [records, setRecords] = useState(KioskService.getAllKiosks())
     const [filterFunction, setFilterFunction] = useState({fn: items => { return items; }})
     const [openPopup, setOpenPopup] = useState(false)
+    const [notify, setNotify] = useState({isOpen: false, message:'', type:''})
+    const [confirmModal, setConfirmModal] = useState({isOpen:false, title:''})
 
     const {
         TblContainer,
@@ -54,11 +58,46 @@ function Kiosks() {
         })
     }
 
+    const addOrEdit = (kiosk, resetForm) => {
+        if (kiosk.id == 0) 
+            KioskService.insertKiosk(kiosk)
+        else 
+            KioskService.updateKiosk(kiosk)
+        resetForm()
+        setRecordForEdit(null)
+        setOpenPopup(false)
+        setRecords(KioskService.getAllKiosks())
+        setNotify({
+            isOpen:true,
+            message: 'Submitted Successfully',
+            type: 'success'
+        })
+    }
+
+    const openInPopup = item => {
+        setRecordForEdit(item)
+        setOpenPopup(true)
+    }
+
+    const onDelete = id => {
+        setConfirmModal ({
+            ...confirmModal,
+            isOpen:false
+        })
+        KioskService.deleteKiosk(id);
+        setRecords(KioskService.getAllKiosks())
+        setNotify({
+            isOpen:true,
+            message: 'Deleted Successfully',
+            type: 'error'
+        })
+        
+    }
+
     return(
         <>
             <div className="content">
-                
-                <CreateKioskForm />
+                {/* <CreateKioskForm /> */}
                 <Row>
                     <Col md = "12">
                         <Card>
@@ -72,7 +111,9 @@ function Kiosks() {
                                         <Input type="text" placeholder="Search Location" 
                                          onChange={handleSearch}></Input>
                                     </InputGroup>
-                                    <Button onClick={() => setOpenPopup(true)}>Add New</Button>
+                                    <Button 
+                                        color="success"
+                                        onClick={() => { setOpenPopup(true); setRecordForEdit(null); }}>Create New Kiosk</Button>
                                 </div>
                             </CardHeader>
                             <CardBody>
@@ -87,6 +128,24 @@ function Kiosks() {
                                                     <td>{item.description}</td>
                                                     <td>{item.dateInstalled}</td>
                                                     <td>{item.enabled}</td>
+                                                    <td>
+                                                        <Button size="sm" color="info" 
+                                                            onClick={() => {openInPopup(item)}}>
+                                                            <AiOutlineEdit />
+                                                        </Button>
+                                                        <Button size="sm" color="danger" 
+                                                            onClick={() => {
+                                                                setConfirmModal({
+                                                                    isOpen:true,
+                                                                    title:'Are you sure?',
+                                                                    onConfirm:() => {onDelete(item.id)}
+                                                                })
+                                                                // onDelete(item.id)
+                                                            }}>
+                                                            <AiOutlineClose />
+                                                        </Button>
+                                                        
+                                                    </td>
                                                 </tr>
                                             ))
                                         }
@@ -94,31 +153,22 @@ function Kiosks() {
                                     </tbody>
                                 </TblContainer>
                                 <TblPagination />
-                                {/* <Table responsive>
-                                    <thead className="text-success">
-                                        <tr>
-                                            <th>ID</th>
-                                            <th>Location</th>
-                                            <th>Description</th>
-                                            <th>Date Installed</th>
-                                            <th>Enabled</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>1</td>
-                                            <td>Seng Kang Compass One</td>
-                                            <td>First level outside the sliding door</td>                                         
-                                            <td>28/11/20</td>
-                                            <td>Enabled</td>
-                                        </tr>
-                                    </tbody>
-                                </Table> */}
                             </CardBody>
-                            <Popup openPopup = {openPopup}
+                            <Popup 
+                            title="Create New Kiosk Form"
+                            openPopup = {openPopup}
                             setOpenPopup = {setOpenPopup}>
+                                <CreateKioskForm 
+                                recordForEdit={recordForEdit}
+                                addOrEdit={addOrEdit} />
 
                             </Popup>
+                            <Noti 
+                            notify={notify}
+                            setNotify={setNotify}/>
+                            <ConfirmModal 
+                            confirmModal={confirmModal}
+                            setConfirmModal={setConfirmModal}/>
                         </Card>
                     </Col>
                 </Row>
